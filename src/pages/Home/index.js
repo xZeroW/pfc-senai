fec554e596f5aef296a5fc506bc1cc5b0e9aa35e
xZeroW <vic_marts@hotmail.com>
2020-07-16 16:32:37 -0300

    homezinha

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,13 +3,16 @@ import React from 'react';
 import { Container } from 'components/Grid/styles';
 import Navbar from 'components/Navbar';
 
+import { HomeImage } from './styles';
+import homeImg from 'assets/img/Home.png';
+
 export default function Home() {
 
   return(
     <>
       <Navbar />
       <Container>
-        <h1>Home</h1>
+        <HomeImage src={homeImg} alt='home' />
       </Container>
     </>
   );
